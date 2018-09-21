36b59c8cd6a0fc1cfde52cddaec61f3e590d4205
Finix <graciecommander@icloud.com>
2018-09-21 18:30:57 -0400

    added a practice file in order to better understand python, ran through a few simple exercises

---
+++ Intro.py
@@ -0,0 +1,21 @@
+# March of the Potato Fury (Or variables and print statements)
+potato_fury = "Chris"
+
+print("Hello! My name is " + potato_fury)
+print("Being me, " + potato_fury + ", is really awesome.")
+print("If only, everyone was named " + potato_fury + "...")
+print("Then it would be a whole world of " + potato_fury + "'s!")
+
+# Write code
+# Step 1: That creates a variable that holds someone's age
+age = int(input("How old are you? (please use digits) "))
+
+# Step 2: Add ten to the person's age
+new_age = age + 10
+
+# Step 3: Create a print statement that tells the user how old they'll be in ten years
+print("In ten years you will be this old: " + str(new_age))
+
+super_new_age = age + 15
+
+print("In fifteen years you will be this old: " + str(super_new_age))
