2474495c83ef8e1e9d59b2e2dee623fdeefd37ad
Finix <graciecommander@icloud.com>
2018-09-21 16:46:15 -0400

    Adding initial test file

---
+++ Testfile.py
@@ -0,0 +1,15 @@
+name = "John"
+
+country = "United States"
+
+age = 24
+
+print("Hello " + name + "!")
+
+print("You live in the " + country)
+
+print("You are " + str(age) + " years old")
+
+somevar = age * age
+
+print(somevar)
